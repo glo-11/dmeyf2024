7b3e991f5c29b8bb77e6c187eb7bfc1daddcd34d
glo-11 <valenciag17@gmail.com>
2024-11-22 12:38:55 +0000

    .

--- src/workflows/m918_workflow_base_f202108.r
+++ src/workflows/m918_workflow_base_f202108.r
@@ -425,7 +425,7 @@ wf_agosto <- function( pnombrewf )
   param_local <- exp_wf_init( pnombrewf ) # linea workflow inicial fija
 
   # Etapa especificacion dataset de la Segunda Competencia Kaggle
-  DT_incorporar_dataset( "~/buckets/b1/datasets/competencia_02.csv.gz")
+  DT_incorporar_dataset( "~/buckets/b1/datasets/competencia_02_nuevo.csv")
 
   # Etapas preprocesamiento
   CA_catastrophe_base( metodo="MachineLearning")
